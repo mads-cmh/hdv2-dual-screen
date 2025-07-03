d827dc6e330d6fbdd0118db09ad6b6fda71ee177
mads-cmh <mads.jensen@copmer.com>
2025-07-03 16:08:07 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -1,12 +1,14 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
-
-local screen1 = resource.render_child("playlist1")
-local screen2 = resource.render_child("playlist2")
+local font = resource.load_font("default.ttf")
 
 function node.render()
-  gl.viewport(0, 0, WIDTH / 2, HEIGHT)
-  screen1:draw(0, 0, WIDTH / 2, HEIGHT)
+  gl.clear(0, 0, 0, 1)
 
+  gl.color(1, 0, 0, 1)
+  gl.viewport(0, 0, WIDTH / 2, HEIGHT)
+  font:write(100, 100, "LEFT DISPLAY", 80, 1, 1, 1, 1)
+
+  gl.color(0, 0, 1, 1)
   gl.viewport(WIDTH / 2, 0, WIDTH / 2, HEIGHT)
-  screen2:draw(WIDTH / 2, 0, WIDTH / 2, HEIGHT)
+  font:write(100, 100, "RIGHT DISPLAY", 80, 1, 1, 1, 1)
 end
