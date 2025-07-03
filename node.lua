e8473bf6ba03dcac2af483b595cf546220115577
mads-cmh <mads.jensen@copmer.com>
2025-07-03 16:18:03 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -1,13 +1,12 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
+local screen1 = resource.render_child("playlist1")
+local screen2 = resource.render_child("playlist2")
+
 function node.render()
-  gl.clear(0, 0, 0, 1)
-
-  gl.color(1, 0, 0, 1)
   gl.viewport(0, 0, WIDTH / 2, HEIGHT)
-  gl.rect(0, 0, WIDTH / 2, HEIGHT)
+  screen1:draw(0, 0, WIDTH / 2, HEIGHT)
 
-  gl.color(0, 0, 1, 1)
   gl.viewport(WIDTH / 2, 0, WIDTH / 2, HEIGHT)
-  gl.rect(0, 0, WIDTH / 2, HEIGHT)
+  screen2:draw(WIDTH / 2, 0, WIDTH / 2, HEIGHT)
 end
